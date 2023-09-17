9c864dcb39e818307f995dc498f36379949990f4
Alfredowo <112134780+Alfredowo@users.noreply.github.com>
2023-09-16 20:01:21 -0600

    funcion para que los admins solo se puedan logiar con una cuenta de admin y no con una cuenta de usuario

--- FormularioUsuario.php
+++ FormularioUsuario.php
@@ -6,61 +6,73 @@
 </head>
 <body>
     <h1>Subir Documentos</h1>
-    <h2>Certificacion</h2>
+    <h2>Certificado de grado de estudios</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion 2</h2>
+    <h2>Comprobante de años de antiguedad</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de Horas de cursos de capacitación impartidas</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de certificaciones</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de diplomados</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de cursos de ST</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobande de Horas impartidas de cursos</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de instructor de diplomados</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de instructor de certificaciones</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
     </form>
     <br>
-    <h2>Certificacion</h2>
+    <h2>Comprobante de No. de veces que ha sido asesor de residencias</h2>
+    <form action="procesar.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="archivos[]" multiple>
+        <input type="submit" value="Subir Documentos">
+    </form>
+    <br>
+    <h2>Comprobante de No. de veces que ha sido asesor de titulación</h2>
+    <form action="procesar.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="archivos[]" multiple>
+        <input type="submit" value="Subir Documentos">
+    </form>
+    <br>
+    <h2>Comprobante de dirección de tesis</h2>
     <form action="procesar.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivos[]" multiple>
         <input type="submit" value="Subir Documentos">
