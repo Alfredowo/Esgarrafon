3df456b67f648d83be7e61953b7b1c3004fc4cae
Alfredowo <112134780+Alfredowo@users.noreply.github.com>
2023-09-18 20:02:35 -0600

    finalizado registrar admins

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -2,19 +2,29 @@
 // Conectar a la base de datos (asegúrate de establecer la conexión adecuadamente)
 require("conexion.php");
 
+// Encabezado para permitir solicitudes desde cualquier origen (CORS)
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
 // Obtener los datos enviados desde el cliente
-$usuario = $_POST['usuario'];
-$contrasena = $_POST['contrasena'];
+$usuario = $_POST['registroUsuario'];
+$contrasena = $_POST['registroContrasena'];
+$permisos = $_POST['permisos'];
 
-// Realizar una consulta SQL para insertar el nuevo usuario en la base de datos
-$query = "INSERT INTO usuarios VALUES (NULL, '$usuario', '$contrasena',2)";
+//echo json_encode("usuario: $usuario, contraseña: $contrasena");
 
-if (mysqli_query($conexion, $query)) {
+//Realizar una consulta SQL para insertar el nuevo usuario en la base de datos
+$query = "INSERT INTO Login VALUES (NULL, '$usuario', '$contrasena', $permisos)";
+$result = mysqli_query($conn, $query);
+
+if ($result) {
     // Devolver una respuesta JSON indicando que el registro fue exitoso
-    header('Content-Type: application/json');
-    echo json_encode(['registrado' => true]);
+    //header('Content-Type: application/json');
+    //echo json_encode(['registrado' => true]);
+    echo json_encode("registro exioso, $permisos");
 } else {
     // Manejo de errores si la consulta falla
-    echo json_encode(['registrado' => false]);
+    //echo json_encode(['registrado' => false]);
+    echo json_encode("registro fallido");
 }
 ?>
--- verificar_registro.php
+++ verificar_registro.php
@@ -2,22 +2,27 @@
 // Conectar a la base de datos (asegúrate de establecer la conexión adecuadamente)
 require("conexion.php");
 
+// Encabezado para permitir solicitudes desde cualquier origen (CORS)
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
 // Obtener el nombre de usuario enviado desde el cliente
-$usuario = $_POST['usuario'];
+$usuario = $_POST['registroUsuario'];
+//echo "el nombre que se obtuvo es: $usuario";
 
 // Realizar una consulta SQL para verificar si el usuario existe en la base de datos
-$query = "SELECT COUNT(*) AS existe FROM usuarios WHERE Usuario = '$usuario'";
-$resultado = mysqli_query($conexion, $query);
+$query = "SELECT COUNT(*) AS existe FROM Login WHERE Usuario = '$usuario'";
+$resultado = mysqli_query($conn, $query);
 
+// Verificar si el usuario se recibió correctamente
 if ($resultado) {
     $fila = mysqli_fetch_assoc($resultado);
     $existe = ($fila['existe'] > 0) ? true : false;
-
     // Devolver una respuesta JSON con el resultado
     header('Content-Type: application/json');
     echo json_encode(['existe' => $existe]);
 } else {
-    // Manejo de errores si la consulta falla
-    echo json_encode(['existe' => false]); // Supongamos que no existe en caso de error
+    // Manejo de excepciones si la consulta falla
+    echo json_encode(['existe' => false]); // Supongamos que no existe 
 }
 ?>
